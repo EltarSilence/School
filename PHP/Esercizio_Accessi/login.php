05c725471d11b204a823aa3fc1f6f71acb509ef3
Giovanni Rizza <rizza67jorge@gmail.com>
2019-02-11 13:31:53 +0100

    Minor update

--- PHP/Esercizio_Accessi/login.php
+++ PHP/Esercizio_Accessi/login.php
@@ -13,6 +13,21 @@
     include 'menu.php';
     //mostra form login
     ?>
+    <form action="" method="post" name="loginForm">
+      <table class="login-box">
+        <tr>
+          <td>Username</td>
+          <td><input name="unm" /></td>
+        </tr>
+        <tr>
+          <td>Password</td>
+          <td><input type="password" name="pwd"/></td>
+        </tr>
+        <tr>
+          <td><input type="submit" value="Login" /></td>
+        </tr>
+      </table>
+    </form>
     Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
     <?php
   }
